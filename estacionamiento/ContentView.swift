ee76c2ca8fe4d7326dfa1d8dcf3534029784feff
nestormoya <68744829+nestormoya@users.noreply.github.com>
2020-07-26 19:53:09 -0500

    Initial Commit

---
+++ estacionamiento/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  estacionamiento
+//
+//  Created by Nestor Moya Yepes - Ceiba Software on 26/07/20.
+//  Copyright © 2020 Nestor Moya Yepes - Ceiba Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
